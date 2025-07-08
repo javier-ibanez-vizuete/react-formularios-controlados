a86e18b9dc3a5b38f50d70a38ad7c3882e60a96b
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-07-08 13:19:22 +0200

    feat(app): add the input to show the name with Enter key or button Done

--- src/components/ContactForm/InputName/InputName.jsx
+++ src/components/ContactForm/InputName/InputName.jsx
@@ -3,17 +3,37 @@ import "./InputName.css";
 
 export const InputName = () => {
 	const [text, setText] = useState("");
+	const [visible, setVisible] = useState(false);
 	const onInputChange = ({ target: { value } }) => {
+		if (!value.length) {
+			setVisible(false);
+		}
 		setText(value);
-		console.log(value);
 	};
 
+	const onInputKeyDown = ({ key }) => {
+		if (key !== "Enter") return;
+		return setVisible(!visible);
+	};
+
+	const onButtonClick = () => setVisible(!visible);
+
 	return (
-		<div className="input-name-container">
-			<h2>Texto desde input</h2>
+		<div className="exercise-input-name container">
 			<div className="exercise-container">
-				<input type="text" name="name" id="name" value={text} onChange={onInputChange} />
-				<button onClick={""}>Crear Parrafo</button>
+				<h2>Texto desde input</h2>
+				<label htmlFor="name">Crear nombre</label>
+				<input
+					type="text"
+					name="name"
+					id="name"
+					value={text}
+					onChange={onInputChange}
+					placeholder="Introduce un nombre para crearlo"
+					onKeyDown={onInputKeyDown}
+				/>
+				<button onClick={onButtonClick}>Crear nombre</button>
+				{visible && <h4>{text}</h4>}
 			</div>
 		</div>
 	);
